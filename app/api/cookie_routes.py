6e94912a749f3d607025c8c4f2228609e4e9b68d
Hemza <hezmam6@gmail.com>
2024-08-21 14:06:48 -0400

    adding cookies routes

---
+++ app/api/cookie_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import db, Cookie
+
+#Get All Cookies
+
+#Get current users cookies
+
+#Post a new Cookie
+
+#Edit a Cookie
+
+#delete a Cookie
