f384fc9d4174cfb6dccc51d3c13f969d09dd8eaa
Hemza <hezmam6@gmail.com>
2024-08-21 20:14:13 -0400

    got GET cookies to work in backend routes

--- app/api/cookie_routes.py
+++ app/api/cookie_routes.py
@@ -2,9 +2,25 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import db, Cookie
 
-#Get All Cookies
+cookie_routes = Blueprint('cookies', __name__)
 
-#Get current users cookies
+#Get All Cookies
+@cookie_routes.route('/', methods=['GET'])
+# @login_required
+def get_all_cookies():
+    """
+    Get all cookies that exist in the db
+    """
+    cookies = Cookie.query.all()
+    return jsonify([cookie.to_dict() for cookie in cookies])
+
+#Get cookie by id
+@cookie_routes.route('/<int:id>/', methods=['GET'])
+# @login_required
+def get_cookie_by_id(id):
+    """
+    Returns a cookie by specific id
+    """
 
 #Post a new Cookie
 
