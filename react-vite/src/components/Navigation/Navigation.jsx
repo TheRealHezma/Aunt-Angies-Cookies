dccd96700b18988f44f9688ea10aa2d37f3c1ff7
Hemza <hezmam6@gmail.com>
2024-09-12 12:02:15 -0400

    added logo

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -2,30 +2,35 @@ import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-import logo from '../../../public/Updatedlogo.png'
+import logo from '../../../public/Updatedlogo.png'; // Import the logo image
+
 function Navigation() {
   const user = useSelector((state) => state.session.user); // Access the user from the session state
 
   return (
     <nav className="navigation-bar">
       <ul>
-        <li>
-          <NavLink to="/">About</NavLink>
+        <li className="nav-logo-container">
+          {user ? (
+            <NavLink to="/cookies">
+              <img src={logo} alt="Logo" className="nav-logo" /> {/* Logo links to cookies page if user is logged in */}
+            </NavLink>
+          ) : (
+            <img src={logo} alt="Logo" className="nav-logo" />
+          )}
         </li>
         <li>
-          <a
-            href="https://www.facebook.com/Aunt-Angies-Cookies-and-Co-100094284582480/?paipv=0&eav=AfZOrf9UNId3dD9uPDQDDA16zg69UQ_7sNPjbfMTYfHE3tPJAtfUKTmfdiODkEd_OUw&_rdr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook
-          </a>
+          <div className="nav-links">
+            <NavLink to="/">About</NavLink>
+            <a
+              href="https://www.facebook.com/Aunt-Angies-Cookies-and-Co-100094284582480/?paipv=0&eav=AfZOrf9UNId3dD9uPDQDDA16zg69UQ_7sNPjbfMTYfHE3tPJAtfUKTmfdiODkEd_OUw&_rdr"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </a>
+          </div>
         </li>
-        {user && ( // Only render this if the user is logged in
-          <li>
-            <NavLink to="/cookies">All Cookies</NavLink>
-          </li>
-        )}
         <li>
           <ProfileButton />
         </li>
