9d7c1bd50f93de38c35bf086ad359f56e6583c7d
Hemza <hezmam6@gmail.com>
2025-03-18 17:47:05 -0400

    fixed relations

--- app/models/cookie.py
+++ app/models/cookie.py
@@ -17,7 +17,7 @@ class Cookie(db.Model):
     updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
     # Relationship with reviews
-    reviews = db.relationship('Review', backref='cookie_reviews', lazy=True)
+    reviews = db.relationship('Review', backref='cookie', lazy=True)
 
 
 
